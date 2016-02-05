f2c77634d6f9215fa7a8e9d5f5bbe2f70821f1d3
Brandon Walsh <walshb@wlu.edu>
2016-02-05 15:31:15 -0500

    zot-coins test

--- js/zot-coins.js
+++ js/zot-coins.js
@@ -4,5 +4,5 @@ api_key = "n06ygnguCClUjQgpH2XKsNGG"
 
 jQuery.get("https://api.zotero.org/groups/469500/collections/UC9DKBJN/items/top?format=coins&v=1&key=n06ygnguCClUjQgpH2XKsNGG", function( data ){
   $(".result").html( data );
-  $(document.body).append(data);
+  $('head').append(data);
 });
