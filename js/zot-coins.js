e1b8e426b6f535d99fe28c7c6bbed6adb8e829a8
Brandon Walsh <walshb@wlu.edu>
2016-02-05 16:05:59 -0500

    zot-coins tinkering

--- js/zot-coins.js
+++ js/zot-coins.js
@@ -1,8 +1,9 @@
 group_id = "469500"
 collection_id = "UC9DKBJN"
 api_key = "n06ygnguCClUjQgpH2XKsNGG"
-
-jQuery.get("https://api.zotero.org/groups/469500/collections/UC9DKBJN/items/top?format=coins&v=1&key=n06ygnguCClUjQgpH2XKsNGG", function( data ){
+format = 'rdf_zotero'
+jQuery.get("https://api.zotero.org/groups/469500/collections/UC9DKBJN/items/top?format=coins&v=3&key=n06ygnguCClUjQgpH2XKsNGG", function( data ){
   $(".result").html( data );
+  data
   $('head').append(data);
 });
