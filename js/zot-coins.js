7f053db3c8ac5a3cd63860cd3020a0af1ef6fcad
Brandon Walsh <bmw9t@virginia.edu>
2016-02-08 13:20:38 -0500

    implements COinS metadata and cleans things up.

--- js/zot-coins.js
+++
@@ -1,8 +0,0 @@
-group_id = "469500"
-collection_id = "UC9DKBJN"
-api_key = "n06ygnguCClUjQgpH2XKsNGG"
-
-jQuery.get("https://api.zotero.org/groups/469500/collections/UC9DKBJN/items/top?format=coins&v=1&key=n06ygnguCClUjQgpH2XKsNGG", function( data ){
-  $(".result").html( data );
-  $(document.body).append(data);
-});
