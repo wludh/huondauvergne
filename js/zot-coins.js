fc8c48cee3489e1253b383799ccfbaff26a4f88f
Brandon Walsh <walshb@wlu.edu>
2016-02-09 08:04:51 -0500

    deletes zot-coins

--- js/zot-coins.js
+++
@@ -1,9 +0,0 @@
-group_id = "469500"
-collection_id = "UC9DKBJN"
-api_key = "n06ygnguCClUjQgpH2XKsNGG"
-format = 'rdf_zotero'
-jQuery.get("https://api.zotero.org/groups/469500/collections/UC9DKBJN/items/top?format=coins&v=3&key=n06ygnguCClUjQgpH2XKsNGG", function( data ){
-  $(".result").html( data );
-  data
-  $('head').append(data);
-});
