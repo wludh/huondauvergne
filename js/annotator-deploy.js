aa540fd600a5df734635045adbc087bc840367af
Brandon Walsh <bmw9t@virginia.edu>
2016-02-15 11:14:54 -0500

    initial stab at loading annotations from API

--- js/annotator-deploy.js
+++ js/annotator-deploy.js
@@ -1,5 +1,28 @@
-$('body').annotator()
-             .annotator('setupPlugins');
+// $('body').annotator()
+//              .annotator('setupPlugins');
+
+var content = $('body').annotator();
+    content.annotator('addPlugin', 'Store', {
+      // The endpoint of the store on your server.
+      prefix: 'http://annotateit.org/api/search?user=walshb',
+  	
+      // Attach the uri of the current page to all annotations to allow search.
+      annotationData: {
+        'uri': 'http://huon-staging.herokuapp.com/bibliography.html'
+      },
+
+      // This will perform a "search" action when the plugin loads. Will
+      // request the last 20 annotations for the current url.
+      // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
+      loadFromSearch: {
+        'limit': 20,
+        'uri': 'http://huon-staging.herokuapp.com/bibliography.html'
+      }
+    });
+
+// "http://annotateit.org/api/search?user=walshb" will return all the 
+// annotations by a user. but then how do i load that into the annotator?
+
 
 // $(document).ready(function() {
 //   // Call Annotator JS
